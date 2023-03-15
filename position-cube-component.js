10bfd7d521b0decf8c56f638eb5c8d21d285345c
Matt Arnold <matt.mattarn@gmail.com>
2023-03-15 17:48:28 -0400

    Shrink cubes

--- position-cube-component.js
+++ position-cube-component.js
@@ -5,9 +5,15 @@ AFRAME.registerComponent('position-cube', {
   init: function () {
     var position = this.data.position;
     var id = `cube-${position.x}-${position.y}-${position.z}`;
-    this.el.setAttribute('id', id);
-    this.el.setAttribute('position', position);
-    this.el.setAttribute('wireframe', 'true');
-    this.el.setAttribute('geometry', 'primitive: box; width: 0.9; height: 0.9; depth: 0.9');
+    var el = el;
+    el.setAttribute('id', id);
+    el.setAttribute('position', position);
+    el.setAttribute('wireframe', 'true');
+    el.setAttribute('geometry', {
+      primitive: 'box',
+      width: 0.3,
+      height: 0.3,
+      depth: 0.3
+    });
   }
 });
