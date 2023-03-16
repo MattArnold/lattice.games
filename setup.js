c3b05ff50a530259074b8fe67f350fc018a0f6d7
Matt Arnold <matt.mattarn@gmail.com>
2023-03-16 15:27:26 -0400

    Adjust size of cubes

--- setup.js
+++ setup.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const height = 5;
   const depth = 7;
 
-  const size = 0.8;
+  const size = 0.5;
 
   // Loop through the positions and create a cube for each one
   for (let x = 0; x < width; x++) {
